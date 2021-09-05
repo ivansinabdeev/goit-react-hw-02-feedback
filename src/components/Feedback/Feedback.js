7065ff59134b66f84c65c43a9dafce3a9452a1e8
ivansinabdeev <ivansinabdeev@gmail.com>
2021-09-05 22:46:24 +0200

    fixed code

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,55 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Feedback.css";
 
-class Feedback extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
   clickGood = () => {
-    console.log("its good");
+    this.setState((prevGood) => ({
+      good: prevGood.good + 1,
+    }));
   };
   clickNeutral = () => {
-    console.log("its neutral");
+    this.setState((prevNeutral) => ({
+      neutral: prevNeutral.neutral + 1,
+    }));
   };
   clickBad = () => {
-    console.log("its bad");
+    this.setState((prevBad) => ({
+      bad: prevBad.bad + 1,
+    }));
   };
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total ? total : 0;
+  }
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const percentege = Math.round((good / this.countTotalFeedback()) * 100);
+    return percentege;
+  }
 
   render() {
+    const { good, neutral, bad } = this.state;
+
+    const TotalFeedback = this.countTotalFeedback;
+    const PositiveFeedbackPercentage = this.countPositiveFeedbackPercentage;
+
     return (
       <div>
         <h1>Please leave feedback</h1>
         <div>
-          <button type="button" onClick={this.clickGood}>
+          <button className="button" type="button" onClick={this.clickGood}>
             Good
           </button>
-          <button type="button" onClick={this.clickNeutral}>
+          <button className="button" type="button" onClick={this.clickNeutral}>
             Neutral
           </button>
-          <button type="button" onClick={this.clickBad}>
+          <button className="button" type="button" onClick={this.clickBad}>
             Bad
           </button>
         </div>
-        <h2>Statistics</h2>
+
         <div className="stat">
-          <span className="comment">
+          <h2>Statistics</h2>
+          <p className="comment">
             Good:
             <span>{this.state.good}</span>
-          </span>
-          <span className="comment">
+          </p>
+          <p className="comment">
             Neutral:
             <span>{this.state.neutral}</span>
-          </span>
-          <span className="comment">
+          </p>
+          <p className="comment">
             Bad:
             <span>{this.state.bad}</span>
-          </span>
+          </p>
+          <p className="comment">
+            Total:
+            <span>{TotalFeedback}</span>
+          </p>
+          <p className="comment">
+            Positive feedback:
+            <span>{PositiveFeedbackPercentage}%</span>
+          </p>
         </div>
       </div>
     );
